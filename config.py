33c397fed66f96c99ae6682fec94c553231c14ef
Prashant Khanduri <prashant.khanduri@gmail.com>
2016-03-07 00:06:46 -0800

    Adding support for bootswatch config

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 BOOTSWATCH_TEMPLATE_LIST = ["paper", "sandstone", "cosmo", "darkly", "yeti", "slate", "superhero"]
-BOOTSWATCH_TEMPLATE = BOOTSWATCH_TEMPLATE_LIST[5]
+BOOTSWATCH_TEMPLATE_INDEX = int(os.environ.get('BOOTSWATCH_TEMPLATE_INDEX', '0'))
+BOOTSWATCH_TEMPLATE = BOOTSWATCH_TEMPLATE_LIST[BOOTSWATCH_TEMPLATE_INDEX]
 
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
